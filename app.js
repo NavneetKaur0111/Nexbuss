cc032792b7f3f7ed0e0238f19d04342356b57b6c
navneet <navneet3265@gmail.com>
2020-12-17 18:38:31 -0600

    display bus stop schedules

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const streetNameBox = document.querySelector('#street-name');
 const streetsResultSection = document.querySelector('.streets');
 const schedulebusessection = document.querySelector('tbody');
 const baseUrl = 'https://api.winnipegtransit.com/v3/';
-const apiKey = 'api-key=2T4MALF1Wx9A3YtpUmKz';
+const apiKey = 'api-key=5VCDsFRAs7Mgh62H8dNp';
 
 const findRelatedStreets = (name) => {
   return fetch(`${baseUrl}streets.json?${apiKey}&name=${name}&usage=long`)
@@ -16,7 +16,7 @@ const showRelatedStreets = (name) => {
     .then(data => data.streets)
     .then((streets) => {
       streetsResultSection.innerHTML="";
-      
+
       if(streets.length !== 0) {
         streets.forEach((street) => {
           streetsResultSection.insertAdjacentHTML('beforeend', 
@@ -29,10 +29,85 @@ const showRelatedStreets = (name) => {
     });
 }
 
+const getstopsList = (keys) => {
+  const stopsList = keys.map((key) => {
+    return fetch(`${baseUrl}stops/${key}/schedule.json?${apiKey}&max-results-per-route=2`)
+    .then((data) => data.json())
+    .then(data => (data['stop-schedule']))
+  })
+  return stopsList;
+}
+
+const findStopKeys = (streetKey) => {
+  return fetch(`${baseUrl}stops.json?${apiKey}&street=${streetKey}`)
+  .then((data) => data.json())
+  .then(data => data.stops)
+  .then(stops => {
+        stopsKey = stops.map(stop => stop.key)
+        return stopsKey;
+  });
+}
+
+const getScheduleBusesData = (stopsList) => {
+  stopsList.forEach(stop => {
+    const routes = stop['route-schedules'];
+
+    routes.forEach(route => {
+      const nextBuses = route['scheduled-stops'];
+
+      nextBuses.forEach(bus => {
+        let date = new Date(bus.times.arrival.scheduled);
+         const busData = {
+          name : stop.stop.street.name,
+          crossStName : stop.stop["cross-street"].name,
+          direction : stop.stop.direction,
+          busId : route.route.key,
+          busTime: `${date.getHours()%12 < 10 ? '0' : ''}${date.getHours() % 12}:${date.getMinutes()} ${date.getHours() < 12 ? "AM" : "PM" }`
+        }
+         showScheduleBuses(busData);
+      })   
+    })
+
+
+  })
+}
+
+const showScheduleBuses = (busData) => {
+  schedulebusessection.insertAdjacentHTML('beforeend', 
+  `<tr>
+  <td>${busData.name}</td>
+  <td>${busData.crossStName}</td>
+  <td>${busData.direction}</td>
+  <td>${busData.busId}</td>
+  <td>${busData.busTime}</td>
+  </tr>`)
+}
+
+const findStopRoutes = (streetKey) => {
+  findStopKeys(streetKey)
+  .then((stopKeys) => {
+    return getstopsList(stopKeys)
+  })
+  .then((stopsList) => {
+    Promise.all(stopsList)
+    .then(stopsList => getScheduleBusesData(stopsList));
+  })
+}
+
 inputForm.addEventListener('submit', (event) => {
   event.preventDefault();
   if (event.target.nodeName === 'FORM') {
+    streetsResultSection.innerHTML = '';
+    schedulebusessection.innerHTML = '';
     showRelatedStreets(searchbarInput.value);
     searchbarInput.value= '';
   };
+})
+
+streetsResultSection.addEventListener('click', (event) => {
+  if(event.target.nodeName === 'A') {
+    streetNameBox.innerHTML = `Displaying results for ${event.target.innerHTML}`
+    schedulebusessection.innerHTML = '';
+    findStopRoutes(event.target.dataset.streetKey)
+  }
 })
